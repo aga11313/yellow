7e9e0afab1022d5b5dece96d10879918659eed1e
Cameron MacLeod <cmacleod170@googlemail.com>
2017-02-11 19:34:54 +0000

    Start IOPinService and template changes
    Add config.json
    Put main.cpp into template form
    Ignore YouCompleteMe vim plugin file
    No longer call the button sampling code

--- microbit/source/main.cpp
+++ microbit/source/main.cpp
@@ -1,14 +1,50 @@
+/*********************************************************************
+ * FILENAME: main.cpp
+ *
+ * DESCRIPTION: The entry point of the powerglove application.
+ *
+ * AUTHOR: Cameron MacLeod
+ ********************************************************************/
+
+/*********************************************************************
+ * Includes
+ ********************************************************************/
+
 #include "MicroBit.h"
 
+/*********************************************************************
+ * Private defines
+ ********************************************************************/
+
+// None
+
+/*********************************************************************
+ * Private function prototypes
+ ********************************************************************/
+
+// None
+
+/*********************************************************************
+ * Private data
+ ********************************************************************/
+
 MicroBit uBit;
+
+/*********************************************************************
+ * Public function implementations
+ ********************************************************************/
+
+// None
+
+/*********************************************************************
+ * Private function implementations
+ ********************************************************************/
 
 void scroll(int pinNum) {
     uBit.display.scroll(pinNum);
 }
 
-int main() {
-    uBit.init();
-
+void buttonLoop(void) {
     MicroBitPin p0(MICROBIT_ID_IO_P0, MICROBIT_PIN_P0, PIN_CAPABILITY_DIGITAL);
     MicroBitPin p1(MICROBIT_ID_IO_P1, MICROBIT_PIN_P1, PIN_CAPABILITY_DIGITAL);
     MicroBitPin p2(MICROBIT_ID_IO_P2, MICROBIT_PIN_P2, PIN_CAPABILITY_DIGITAL);
@@ -33,3 +69,13 @@ int main() {
         uBit.sleep(10);
     }
 }
+
+int main() {
+    uBit.init();
+
+    new MicroBitIOPinService(*uBit.ble, uBit.io);
+
+    release_fiber();
+}
+
+/* END OF FILE */
